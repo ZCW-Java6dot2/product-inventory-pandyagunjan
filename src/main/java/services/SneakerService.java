1f09d58befc6dcafdfa1297ad1c1f98a0da49e02
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-05 06:19:22 -0500

    adding 2 files

--- src/main/java/services/SneakerService.java
+++ src/main/java/services/SneakerService.java
@@ -1,4 +1,14 @@
-package services;
+
+import models.Sneaker;
+import java.util.ArrayList;
 
 public class SneakerService {
+
+    private static int nextId = 1;  // (1)
+
+    private AList<Sneaker> inventory = new ArrayList<>();  // (2)
+
+    public SneakerService() {
+    }
+
 }
--- src/test/java/services/SneakerServiceTest.java
+++ src/test/java/services/SneakerServiceTest.java
@@ -1,4 +1,45 @@
 package services;
 
+import models.Sneaker;
+import services.
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SneakerServiceTest {
+
+    @Test
+    public void createTest(){
+
+        // (1)
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        String expectedSport = "Tennis";
+        int expectedSize = 10;
+        int expectedQty = 10;
+        float expectedPrice = 80.00f;
+
+        // (2)
+        SneakerService sneakerService = new SneakerService();
+        Sneaker testSneaker = SneakerService.create(expectedName, expectedBrand,
+                expectedSport, expectedSize, expectedQty, expectedPrice);
+
+        // (3)
+        int actualId = testSneaker.getId();
+        String actualName = testSneaker.getName();
+        String actualBrand = testSneaker.getBrand();
+        String actualSport = testSneaker.getSport();
+        int actualSize = testSneaker.getSize();
+        int actualQty = testSneaker.getQty();
+        float actualPrice = testSneaker.getPrice();
+
+        // (4)
+        Assert.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedBrand, actualBrand);
+        Assert.assertEquals(expectedSport, actualSport);
+        Assert.assertEquals(expectedSize, actualSize);
+        Assert.assertEquals(expectedQty, actualQty);
+        Assert.assertEquals(expectedPrice, actualPrice,0);
+
+    }
 }
