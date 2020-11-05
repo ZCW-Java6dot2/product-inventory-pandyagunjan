e56e69a48db4bc9986046458edde4f038fa469c5
gunjan <gunjan@zipcodes-mbp-78.lan>
2020-11-05 06:00:57 -0500

    Constrcutor with parameters

--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -1,4 +1,31 @@
-package models;
+import models.Sneaker;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class SneakerTest {
+
+    @Test // (1)
+    public void constructorTest(){
+
+        // (2)
+        int expectedId = 6;
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        String expectedSport = "Tennnis";
+        int expectedQty = 10;
+        float expectedPrice = 80.00f;
+
+
+        // (3)
+        Sneaker testSneaker = new Sneaker(expectedId, expectedName, expectedBrand,
+                expectedSport, expectedQty,expectedPrice);
+
+        // (4)
+        Assert.assertEquals(expectedId, testSneaker.getId());
+        Assert.assertEquals(expectedName, testSneaker.getName());
+        Assert.assertEquals(expectedBrand, testSneaker.getBrand());
+        Assert.assertEquals(expectedSport, testSneaker.getSport());
+        Assert.assertEquals(expectedQty, testSneaker.getQty());
+        Assert.assertEquals(expectedPrice, testSneaker.getPrice(),0);
+    }
 }
